d582453203b3d7666bb5a7cf060cd1eea50a5e7a
Malak Odtalla <malakodt35@gmail.com>
2022-06-19 08:36:14 +0300

    add linked list

---
+++ Linked_List/linked_list/Linked_list.py
@@ -0,0 +1,57 @@
+class Node:
+
+    def __init__(self, value):
+        self.value = value
+        self.next = None
+
+class LinkedList:
+
+    def __init__(self):
+
+        self.head = None
+
+
+    def insert(self, value):
+
+               if value is None:
+                  raise Exception('Please insert value as argument')
+               else:
+
+                if value is None:
+                    raise Exception('Please insert value as argument')
+                else:
+
+                    node = Node(value)
+                    node.next = self.head
+                    self.head = node
+
+    def includes(self, value):
+        current = self.head
+        while current:
+            if current.value == value:
+                return True
+            else:
+                current = current.next
+        return False
+
+    def to_string(self):
+
+        list_str = ''
+        current = self.head
+        while current:
+            # print(current, "current")
+            list_str += f" {current.value}  ->"
+            current = current.next
+        list_str += ' NULL'
+        return list_str
+
+
+if __name__ == '__main__':
+    list = LinkedList()
+    list.insert(5)#
+    list.insert(9)
+
+
+
+
+    print(list.to_string())
---
+++ Linked_List/linked_list/README.md
@@ -0,0 +1,23 @@
+# Singly Linked List
+The most important data structure in the world!
+
+## Code Challenge: 05
+The aim of this code challenge is to make a simple linked list. So the code depends on create a linkedlist as class, with 3 methods like:
+
+- Insert: Adds a new node to the head of the list.
+- Includes: Indicates whether specific value exists as a Node’s value somewhere within the list.
+- ToString: Returns: a string representing all the values in the Linked List.
+
+## Approach & Efficiency
+The Big O for each method used:
+
+- Insert: O(1)
+- Includes: O(n)
+- ToString: O(n)
+
+## API
+- Insert: takes value and return None.
+
+- Includes: takes value and return True/False
+
+- ToString: takes None and return String.
